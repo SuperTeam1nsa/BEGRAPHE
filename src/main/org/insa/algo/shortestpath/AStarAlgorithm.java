5bd70a2026b4ec6106cb462986ff7474bf449103
AbdelSaTd <tarnagda@etud.insa-toulouse.fr>
2019-05-24 21:48:44 +0200

    AStar fastest works !

--- src/main/org/insa/algo/shortestpath/AStarAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/AStarAlgorithm.java
@@ -13,10 +13,19 @@ public class AStarAlgorithm extends DijkstraAlgorithm {
 public void initLabel(Graph graph) {
      // Initialisation du tableau des Labels
        map = new LabelStar[graph.size()];
+        double timeFromDest;
+        double distanceFromDest;
+        
         
         //Remplissage du tableau des labels
         for(Node n:graph.getNodes()) {
-        	map[n.getId()] =new LabelStar(n,Point.distance(n.getPoint(), data.getDestination().getPoint()));
+        	distanceFromDest = Point.distance(n.getPoint(), data.getDestination().getPoint());
+        	if(graph.getGraphInformation().hasMaximumSpeed())
+        		timeFromDest = (distanceFromDest * 3.6) / graph.getGraphInformation().getMaximumSpeed();
+        	else
+        		timeFromDest = (distanceFromDest * 3.6) / 50; // 50 km/h (vitesse moyenne)
+        	
+        	map[n.getId()] =new LabelStar(n, timeFromDest);
         }
 	}
 
