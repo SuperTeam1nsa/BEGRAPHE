c8580ceabde3ff0ed0b54dce86f6a059b7e276fa
AbdelSaTd <tarnagda@etud.insa-toulouse.fr>
2019-05-26 00:06:18 +0200

    AStar adding of consideration of the mode

--- src/main/org/insa/algo/shortestpath/AStarAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/AStarAlgorithm.java
@@ -1,5 +1,6 @@
 package org.insa.algo.shortestpath;
 
+import org.insa.algo.AbstractInputData.Mode;
 import org.insa.graph.Graph;
 import org.insa.graph.Node;
 import org.insa.graph.Point;
@@ -10,24 +11,32 @@ public class AStarAlgorithm extends DijkstraAlgorithm {
         super(data);
     }
     @Override
-public void initLabel(Graph graph) {
-     // Initialisation du tableau des Labels
-       map = new LabelStar[graph.size()];
-        double timeFromDest;
-        double distanceFromDest;
-        
-        
-        //Remplissage du tableau des labels
-        for(Node n:graph.getNodes()) {
-        	distanceFromDest = Point.distance(n.getPoint(), data.getDestination().getPoint());
-        	if(graph.getGraphInformation().hasMaximumSpeed())
-        		timeFromDest = (distanceFromDest * 3.6) / graph.getGraphInformation().getMaximumSpeed();
-        	else
-        		timeFromDest = (distanceFromDest * 3.6) / 50; // 50 km/h (vitesse moyenne)
-        	
-        	map[n.getId()] =new LabelStar(n, timeFromDest);
-        }
-	}
-
+    public void initLabel(Graph graph) {
+	    // Initialisation du tableau des Labels
+	    map = new LabelStar[graph.size()];
+	    double timeFromDest;
+	    double distanceFromDest;
+	    double maxSpeed;
+	     
+	    if(graph.getGraphInformation().hasMaximumSpeed())
+	    	maxSpeed = graph.getGraphInformation().getMaximumSpeed();
+	    else
+	    	maxSpeed = 50; // 50 km/h (vitesse max moyenne)
+     
+     
+	    //Remplissage du tableau des labels
+	    for(Node n:graph.getNodes()) {
+	    	distanceFromDest = Point.distance(n.getPoint(), data.getDestination().getPoint());
+     	
+	     	if(data.getMode() == Mode.LENGTH){
+	     		map[n.getId()] = new LabelStar(n, distanceFromDest);
+	     	}
+	     	else
+	     	{
+	     		timeFromDest = (distanceFromDest * 3.6) / maxSpeed;
+	     		map[n.getId()] =new LabelStar(n, timeFromDest);
+	     	}
+	    }
+    }
 
 }
